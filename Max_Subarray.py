938569689ab4dcaaba2754b536f59deada572798
hkcheckmate2 <47267569+hkcheckmate2@users.noreply.github.com>
2020-08-29 14:43:57 +0530

    Add files via upload
    corrected code

--- Max_Subarray.py
+++ Max_Subarray.py
@@ -1,4 +1,23 @@
-def Kadane(lst):
+def Kadane(A):
+    Length = len(A)
+    Sum = [0]
+    Sum *= Length
+    Sum[0] = A[0]
+    for i in range(1,Length):
+        if Sum[i-1] > 0:
+            Sum[i] = Sum[i-1] + A[i]
+        else:
+            Sum[i] = A[i]
+    Target_Sum = max(Sum)
+    ref = Sum.index(Target_Sum)
+    x = ref
+    s = 0
+    while(s != Target_Sum):
+        s += A[x]
+        x -= 1
+    start = x + 1
+    return A[start:ref+1]
+"""
     max_sum = []
     max_sum.append(lst[0])
     L = len(lst)
@@ -14,6 +33,4 @@ def Kadane(lst):
         x += lst[i]
         if x == target_sum:
             print(a[i:ref+1])
-    
-
-    
+"""
