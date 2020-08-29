759106102679e8435223f52ee06054aac293b9f0
hkcheckmate2 <47267569+hkcheckmate2@users.noreply.github.com>
2020-08-29 22:41:00 +0530

    Add files via upload

---
+++ bst.py
@@ -0,0 +1,38 @@
+class Node(object):
+    def __init__(self, val):
+        self.l_child = None
+        self.r_child = None
+        self.val = val
+
+class BinarySearchTree(object):
+    def insert(self, root, node):
+        if root is None:
+            return node
+        if root.val < node.val:
+            root.r_child = self.insert(root.r_child, node)
+        else:
+            root.l_child = self.insert(root.l_child, node)
+            return root
+
+    def in_order_place(self, root):
+        if root is None:
+            return None
+        else:
+            self.in_order_place(root.l_child)
+            print (root.val)
+            self.in_order_place(root.r_child)
+
+    def pre_order_place(self, root):
+        if root is None:
+            return None
+        else:
+            print (root.val)
+            self.pre_order_place(root.l_child)
+            self.pre_order_place(root.r_child)
+    def post_order_place(self, root):
+        if root is None:
+            return None
+        else:
+            self.post_order_place(root.l_child)
+            self.post_order_place(root.r_child)
+            print (root.val)
