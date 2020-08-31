c7af0e20a9855e4cec974e128e2bdff7b88162af
hkcheckmate2 <47267569+hkcheckmate2@users.noreply.github.com>
2020-08-31 12:04:14 +0530

    Update minSum.py

--- minSum.py
+++ minSum.py
@@ -1,16 +1,16 @@
 import heapq
-import numpy
 import math
   
 def minSum(num,k):
     print(num)
     t = 0
-    num = list(numpy.array(num) * -1)
+    for x in num:
+      x *= -1
     for i in range(k):
         heapq.heapify(num)
         t = heapq.heappop(num)
         heapq.heappush(num,math.floor(t/2))
-    return -sum(num)
+    return sum(num)
 
 L1=[5,7,8,9,4,3]
 print(minSum(L1,4))
