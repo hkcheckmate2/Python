f910ecf1531b688ea09dace9d085211d279d0c91
hkcheckmate2 <47267569+hkcheckmate2@users.noreply.github.com>
2020-08-31 16:01:38 +0530

    Update stair.py

--- stair.py
+++ stair.py
@@ -4,3 +4,26 @@ def climbStairs(n)
     num[0] = num[0] + num[1]
     num[1] = num[1] + num[0]
   return num[(n+1)%2]
+
+"""
+if stepsize = [1,2,4,5,7]
+def climbStairs(n,stepsize)
+  s = len(stepsize)
+  num = [0 for i in range(s+1)]
+  num[0] = 1
+  for i in range(1,s+1):
+    for x in stepsize:
+      num[i] += num[i-x]
+  
+  for i in range(1,n+1,s):
+    for j in range(1,(s+1)%s):
+      num[i] += num[i+j]
+  return num[(n+2)%s]
+  
+  
+  num = [0 for i in range(s)]
+  for i in range(s):
+    for x in stepsize:
+      num[i] += num[i-x]
+   
+"""
